f38357cee670e708a01a01a59c8cf4890a4bea68
Nils van der Werf <n.vanderw.92@gmail.com>
2022-02-28 17:21:39 +1100

    written all sort functions

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,73 +1,45 @@
-
-def sort_array_asc array_of_ints
-    sorted_ints = array_of_ints.sort do |a, b|
-        a <=> b
-    end
-    sorted_ints
+def sort_array_asc integer_array
+    integer_array.sort
 end
 
-def sort_array_desc array_of_ints
-    sorted_ints = array_of_ints.sort do |a, b|
-        b <=> a
-    end
-    sorted_ints
+def sort_array_desc integer_array
+    integer_array.sort {|x, y| y <=> x }
 end
 
-def sort_array_char_count array_of_strings
-    sorted_strings = array_of_strings.sort do |a, b|
-        a.length <=> b.length
-    end
-    sorted_strings
+def sort_array_char_count array 
+    array.sort {|x, y| x.length <=> y.length}
 end
 
-def swap_elements array_of_elements
-   array_of_elements[1], array_of_elements[2] = 
-   array_of_elements[2], array_of_elements[1]
-   array_of_elements
+def swap_elements array 
+    array[1], array[2] = array[2], array[1]
+    array
 end
 
-def swap_elements_from_to array_of_elements, a, b
-    array_of_elements[a], array_of_elements[b] = 
-    array_of_elements[b], array_of_elements[a]
-    array_of_elements
- end
+def reverse_array array 
+    array = array.reverse
+    array
+end
 
- def reverse_array array_of_ints
-    array_copy = array_of_ints.reverse
-    array_copy
- end
- reverse_array ["1", "2", "3", "4", "5"]
-# swap_elements_from_to(["horse", "chicken", "duck", "moose", "pig"], 0, 2)
-# swap_elements_from_to(["horse", "chicken", "duck", "moose", "pig"], 3, 4)
-reverse_array ["1", "2", "3", "4", "5"]
-
-def kesha_maker array
+def kesha_maker array 
     array.each do |item|
-       item[2] = "$"
-    end
-   array
-end
-
-kesha_maker ["blake", "asaley", "scahtt"]
-
-def find_a array
-    array.select do |word|
-        word[0] == "a"
+        item[2] = "$"
     end
 end
 
-find_a ["anagram", "frog", "apple", "apple pie", "potato"]
+def find_a array 
+    array.filter {|item| item.start_with?("a")}
+end 
 
-def sum_array array_of_numbers
-    array_of_numbers.inject do |sum, n| 
-        sum + n
-    end
-end
+def sum_array array 
+    array.inject {|sum, n| sum + n}
+end 
 
-sum_array ["1", "2", "3", "4", "5"]
-
-def add_s array
-    array.each_with_index.collect{|word, n| n != 1 ? word + "s" : word  } 
-end
-
-sum_array ["1", "2", "3", "4", "5"]
+def add_s array 
+    array.collect do |word|
+        if array[1] == word
+            word
+        else 
+            word + "s"
+        end
+    end 
+end 
